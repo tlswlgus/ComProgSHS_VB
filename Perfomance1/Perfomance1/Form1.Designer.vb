15553bb4e065137058f4fc4a04c76183376b2751
Jihyeon <141270839+tlswlgus@users.noreply.github.com>
2023-12-06 02:14:25 +0800

    Add files via upload

---
+++ Perfomance1/Perfomance1/Form1.Designer.vb
@@ -0,0 +1,196 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblname = New System.Windows.Forms.Label()
+        Me.lbltitle = New System.Windows.Forms.Label()
+        Me.lbltaught1 = New System.Windows.Forms.Label()
+        Me.lbltaught2 = New System.Windows.Forms.Label()
+        Me.lbltaught3 = New System.Windows.Forms.Label()
+        Me.bntdisplay = New System.Windows.Forms.Button()
+        Me.lblnum1 = New System.Windows.Forms.Label()
+        Me.lblnum2 = New System.Windows.Forms.Label()
+        Me.lblnum3 = New System.Windows.Forms.Label()
+        Me.picnewyear = New System.Windows.Forms.PictureBox()
+        Me.picIU1 = New System.Windows.Forms.PictureBox()
+        Me.bntclose = New System.Windows.Forms.Button()
+        CType(Me.picnewyear, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.picIU1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblname
+        '
+        Me.lblname.Font = New System.Drawing.Font("Microsoft Sans Serif", 27.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblname.Location = New System.Drawing.Point(-3, -1)
+        Me.lblname.Name = "lblname"
+        Me.lblname.Size = New System.Drawing.Size(416, 43)
+        Me.lblname.TabIndex = 0
+        Me.lblname.Text = "Jihyeon"
+        '
+        'lbltitle
+        '
+        Me.lbltitle.Font = New System.Drawing.Font("Bookman Old Style", 20.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltitle.Location = New System.Drawing.Point(29, 42)
+        Me.lbltitle.Name = "lbltitle"
+        Me.lbltitle.Size = New System.Drawing.Size(414, 36)
+        Me.lbltitle.TabIndex = 1
+        Me.lbltitle.Text = "What does 2020 taught me"
+        '
+        'lbltaught1
+        '
+        Me.lbltaught1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.lbltaught1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltaught1.ForeColor = System.Drawing.Color.Blue
+        Me.lbltaught1.Location = New System.Drawing.Point(81, 90)
+        Me.lbltaught1.Name = "lbltaught1"
+        Me.lbltaught1.Size = New System.Drawing.Size(388, 23)
+        Me.lbltaught1.TabIndex = 2
+        '
+        'lbltaught2
+        '
+        Me.lbltaught2.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbltaught2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltaught2.ForeColor = System.Drawing.Color.Red
+        Me.lbltaught2.Location = New System.Drawing.Point(81, 141)
+        Me.lbltaught2.Name = "lbltaught2"
+        Me.lbltaught2.Size = New System.Drawing.Size(388, 23)
+        Me.lbltaught2.TabIndex = 3
+        '
+        'lbltaught3
+        '
+        Me.lbltaught3.BackColor = System.Drawing.SystemColors.ControlLightLight
+        Me.lbltaught3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbltaught3.ForeColor = System.Drawing.Color.Fuchsia
+        Me.lbltaught3.Location = New System.Drawing.Point(81, 192)
+        Me.lbltaught3.Name = "lbltaught3"
+        Me.lbltaught3.Size = New System.Drawing.Size(388, 23)
+        Me.lbltaught3.TabIndex = 4
+        '
+        'bntdisplay
+        '
+        Me.bntdisplay.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bntdisplay.Location = New System.Drawing.Point(323, 243)
+        Me.bntdisplay.Name = "bntdisplay"
+        Me.bntdisplay.Size = New System.Drawing.Size(181, 74)
+        Me.bntdisplay.TabIndex = 5
+        Me.bntdisplay.Text = "Click here to display It"
+        Me.bntdisplay.UseVisualStyleBackColor = True
+        '
+        'lblnum1
+        '
+        Me.lblnum1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnum1.Location = New System.Drawing.Point(46, 90)
+        Me.lblnum1.Name = "lblnum1"
+        Me.lblnum1.Size = New System.Drawing.Size(29, 26)
+        Me.lblnum1.TabIndex = 6
+        Me.lblnum1.Text = "1."
+        '
+        'lblnum2
+        '
+        Me.lblnum2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnum2.Location = New System.Drawing.Point(46, 141)
+        Me.lblnum2.Name = "lblnum2"
+        Me.lblnum2.Size = New System.Drawing.Size(29, 26)
+        Me.lblnum2.TabIndex = 7
+        Me.lblnum2.Text = "2."
+        '
+        'lblnum3
+        '
+        Me.lblnum3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblnum3.Location = New System.Drawing.Point(46, 189)
+        Me.lblnum3.Name = "lblnum3"
+        Me.lblnum3.Size = New System.Drawing.Size(29, 26)
+        Me.lblnum3.TabIndex = 8
+        Me.lblnum3.Text = "3."
+        '
+        'picnewyear
+        '
+        Me.picnewyear.Image = Global.Perfomance1.My.Resources.Resources.download
+        Me.picnewyear.Location = New System.Drawing.Point(12, 229)
+        Me.picnewyear.Name = "picnewyear"
+        Me.picnewyear.Size = New System.Drawing.Size(167, 109)
+        Me.picnewyear.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picnewyear.TabIndex = 10
+        Me.picnewyear.TabStop = False
+        Me.picnewyear.Visible = False
+        '
+        'picIU1
+        '
+        Me.picIU1.Image = Global.Perfomance1.My.Resources.Resources.FB_IMG_1580196525439
+        Me.picIU1.Location = New System.Drawing.Point(462, -1)
+        Me.picIU1.Name = "picIU1"
+        Me.picIU1.Size = New System.Drawing.Size(95, 88)
+        Me.picIU1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.picIU1.TabIndex = 9
+        Me.picIU1.TabStop = False
+        Me.picIU1.Visible = False
+        '
+        'bntclose
+        '
+        Me.bntclose.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.bntclose.Location = New System.Drawing.Point(323, 243)
+        Me.bntclose.Name = "bntclose"
+        Me.bntclose.Size = New System.Drawing.Size(181, 74)
+        Me.bntclose.TabIndex = 11
+        Me.bntclose.Text = "Click here to Close"
+        Me.bntclose.UseVisualStyleBackColor = True
+        Me.bntclose.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(555, 350)
+        Me.Controls.Add(Me.bntclose)
+        Me.Controls.Add(Me.picnewyear)
+        Me.Controls.Add(Me.picIU1)
+        Me.Controls.Add(Me.lblnum3)
+        Me.Controls.Add(Me.lblnum2)
+        Me.Controls.Add(Me.lblnum1)
+        Me.Controls.Add(Me.bntdisplay)
+        Me.Controls.Add(Me.lbltaught3)
+        Me.Controls.Add(Me.lbltaught2)
+        Me.Controls.Add(Me.lbltaught1)
+        Me.Controls.Add(Me.lbltitle)
+        Me.Controls.Add(Me.lblname)
+        Me.Name = "Form1"
+        Me.Text = "Performace 1"
+        CType(Me.picnewyear, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.picIU1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents lblname As System.Windows.Forms.Label
+    Friend WithEvents lbltitle As System.Windows.Forms.Label
+    Friend WithEvents lbltaught1 As System.Windows.Forms.Label
+    Friend WithEvents lbltaught2 As System.Windows.Forms.Label
+    Friend WithEvents lbltaught3 As System.Windows.Forms.Label
+    Friend WithEvents bntdisplay As System.Windows.Forms.Button
+    Friend WithEvents lblnum1 As System.Windows.Forms.Label
+    Friend WithEvents lblnum2 As System.Windows.Forms.Label
+    Friend WithEvents lblnum3 As System.Windows.Forms.Label
+    Friend WithEvents picIU1 As System.Windows.Forms.PictureBox
+    Friend WithEvents picnewyear As System.Windows.Forms.PictureBox
+    Friend WithEvents bntclose As System.Windows.Forms.Button
+
+End Class
---
+++ Perfomance1/Perfomance1/Form1.vb
@@ -0,0 +1,37 @@
+﻿Public Class Form1
+
+    Private Sub lblname_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lblname.Click
+
+    End Sub
+
+    Private Sub lbltitle_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbltitle.Click
+
+    End Sub
+
+    Private Sub lbltaught1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbltaught1.Click
+
+    End Sub
+
+    Private Sub lbltaught2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbltaught2.Click
+
+    End Sub
+
+    Private Sub lbltaught3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles lbltaught3.Click
+
+    End Sub
+
+    Private Sub bntdisplay_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bntdisplay.Click
+        lbltaught1.Text = "To be resposible"
+        lbltaught2.Text = "That life sucks but you have to deal with it"
+        lbltaught3.Text = "Always be happy"
+        picIU1.Visible = True
+        picnewyear.Visible = True
+        Me.BackColor = Color.Violet
+        bntdisplay.Visible = False
+        bntclose.Visible = True
+    End Sub
+
+    Private Sub bntclose_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles bntclose.Click
+        End
+    End Sub
+End Class
